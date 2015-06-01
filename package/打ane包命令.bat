68c304887de236386ca6346b4120e36516f48d21
liaogang <lghuntfor@sina.com>
2015-06-01 13:55:17 +0800

    update

--- package/打ane包命令.bat
+++ package/打ane包命令.bat
@@ -1,1 +1,4 @@
+echo =========== start package ane ==============
 adt -package -target ane PhoneInfo.ane .\extension.xml  -swc .\PhoneInfo.swc -platform Android-ARM -C Android-ARM .
+echo =========== end package ane ==============
+echo =========== success ==============
